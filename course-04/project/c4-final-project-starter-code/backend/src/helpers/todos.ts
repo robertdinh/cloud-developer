8f9561602ba66009656e2a17dd40c73b5ba31e22
LamDT <dinhtienlam.spkt@gmail.com>
2022-09-15 23:58:26 +0700

    Implement businessLogic

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,10 +1,52 @@
 import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
 
-// TODO: Implement businessLogic
+const logger = createLogger('todos')
+
+const todosAccess = new TodosAccess()
+
+
+export async function createTodo(userId: string, newTodo: CreateTodoRequest): Promise<TodoItem> {
+  const createdAt = new Date().toISOString()  
+  const todoId = uuid.v4()
+  let newItem: TodoItem = {
+    userId,
+    todoId,
+    createdAt,
+    done: false,
+    ...newTodo,
+    attachmentUrl: ''
+  }
+  logger.info('Create Todo: ' + newItem);
+  return await todosAccess.createTodo(newItem)
+}
+
+export async function getTodosPerUser(userId: string): Promise<TodoItem[]> {
+  logger.info('Get List Todos Per User: ' + userId);
+  return todosAccess.getTodosPerUser(userId)
+}
+  
+export async function updateTodo(userId: string, todoId: string, updatedTodo: UpdateTodoRequest): Promise<TodoUpdate> {
+  let todoUpdate: TodoUpdate = {
+    ...updatedTodo
+  }
+  logger.info('Update Todo Item: ' + userId + "," + todoId + "," + todoUpdate);
+  return todosAccess.updateTodo(userId, todoId, todoUpdate)
+}
+
+export async function updateAttachmentUrl(userId: string, todoId: string, attachmentUrl: string): Promise<string> {
+  logger.info('Attachment URL: ' + userId + "," + todoId + "," + attachmentUrl);
+  return todosAccess.updateAttachmentUrl(userId, todoId, attachmentUrl)
+}
+
+export async function deleteTodo(userId: string, todoId: string) {
+logger.info('Delete Todo Item: ' + userId + "," + todoId);
+return todosAccess.deleteTodo(userId, todoId)
+
+}
+
